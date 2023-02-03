8e00b56760d57082ed237c16dbc8737865bc3b08
Pavlos Aslanis <pavlos.aslanis@calluna.se>
2023-02-03 14:50:50 +0100

    Identify greater,smaller

--- Utvecklingsgruppen/Edsviken/Edsviken_report.py
+++ Utvecklingsgruppen/Edsviken/Edsviken_report.py
@@ -9,7 +9,7 @@ import pandas as pd
 import os
 
 # Path of input data.
-path_input_data_raw = os.path.join(os.getcwd(), 'Input' ,'Edsviken - 2023-02-03 14_03.xlsx')
+path_input_data_raw = os.path.join(os.getcwd(), 'Input' ,'Edsviken skogsvik december 2022.xlsx')
 
 # Load data in df.
 input_df_raw = pd.read_excel(path_input_data_raw)
@@ -17,7 +17,6 @@ input_df_raw = pd.read_excel(path_input_data_raw)
 ######################
 ## Fixing the header. 
 ######################
-
 # Save the first row as a list
 first_row = list(input_df_raw.iloc[0])
 # Create a new header by adding the first row to the original header
@@ -34,13 +33,23 @@ for header in merged_header:
     else: 
         fixed_header.append(header.split('/^/||&')[1])
     c+=1
+
+## Deep copy
+input_df_fixedHeaders = input_df_raw.copy()
       
 # Rename the columns with the new header
-input_df_raw.columns = fixed_header
+input_df_fixedHeaders.columns = fixed_header
 
 # Drop the first row
-input_df_raw = input_df_raw.drop(0)
+input_df_fixedHeaders = input_df_raw.drop(0)
 
+
+#####################
+## Identify ">" , "<"
+#####################
+
+mask_smaller = input_df_fixedHeaders.astype(str).apply(lambda x: x.str.contains('<'))
+mask_greater = input_df_fixedHeaders.astype(str).apply(lambda x: x.str.contains('>'))
 
 
 
